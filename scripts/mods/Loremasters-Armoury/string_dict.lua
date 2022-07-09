7250dbe2389b0906615bf3ab8afa3be432f0be12
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-07-09 14:35:18 -0500

    update mod description and fix missing description for bow skin

--- scripts/mods/Loremasters-Armoury/string_dict.lua
+++ scripts/mods/Loremasters-Armoury/string_dict.lua
@@ -47,6 +47,7 @@ local desc_strings = {
     Kerillian_HornOfKurnous_helm_Nightstalker = "Antlered hood, symbolising dedication to Kurnous, God of the Hunt. Seldom worn by elf-maids.",
     Kerillian_HornOfKurnous_helm_Purified = "Showing proper respect to the Pale Queen might call her into acton, and this save your soul from the clutches of chaos.",
     Kerillian_elf_bow_Antlersong_Autumn = "Under the perennially auburn leaves of the mighty guardian Delliandra, the forest grows and grows eternally.",
+    Kerillian_elf_bow_Antlersong_Autumn_runed01 = "Under the perennially auburn leaves of the mighty guardian Delliandra, the forest grows and grows eternally.",
     Kruber_KOTBS_hat = "There is a subtle touch of elven magic upon this helm, protecting its wearer's mind and enhancing their senses. A favour from an old friend?",
     Kruber_KOTBS_armor = "To accomplish any task - to defeat any foe, all that is needed is a keen eye, a sharp mind and the favour of the Goddess.",
     Kruber_Hippogryph_helm_black = "Helmet of an audacious knight, acclaimed valiant after slaying a mighty beast.",
