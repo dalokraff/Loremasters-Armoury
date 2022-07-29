5e6ecd4039636ed54ed891306580608bb543bbf9
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-07-29 12:31:24 -0500

    localized vanilla weapon names

--- scripts/mods/Loremasters-Armoury/Loremasters-Armoury_localization.lua
+++ scripts/mods/Loremasters-Armoury/Loremasters-Armoury_localization.lua
@@ -74,7 +74,15 @@ for id,text in pairs(mod.name_strings_id) do
 	if not mod_text_ids[id] then
 		mod_text_ids[id] = {}
 	end
+	mod_text_ids[id]['zh'] = text
 	mod_text_ids[id]['en'] = text
+	mod_text_ids[id]['fr'] = text
+	mod_text_ids[id]['de'] = text
+	mod_text_ids[id]['it'] = text
+	mod_text_ids[id]['pl'] = text
+	mod_text_ids[id]['br-pt'] = text
+	mod_text_ids[id]['ru'] = text
+	mod_text_ids[id]['es'] = text
 end
 
 
@@ -85,7 +93,15 @@ for _,skin in pairs(mod.vanilla_game_strings) do
 		mod_text_ids[skin] = {}
 	end
 	local translation = game_localize:_base_lookup(ItemMasterList[skin].display_name)--game_localize:_base_lookup(skin_name) or game_localize:_base_lookup("display_name_"..tostring(skin)) or game_localize:_base_lookup(tostring(skin))
+	mod_text_ids[skin]['zh'] = translation
 	mod_text_ids[skin]['en'] = translation
+	mod_text_ids[skin]['fr'] = translation
+	mod_text_ids[skin]['de'] = translation
+	mod_text_ids[skin]['it'] = translation
+	mod_text_ids[skin]['pl'] = translation
+	mod_text_ids[skin]['br-pt'] = translation
+	mod_text_ids[skin]['ru'] = translation
+	mod_text_ids[skin]['es'] = translation
 end
 
 
