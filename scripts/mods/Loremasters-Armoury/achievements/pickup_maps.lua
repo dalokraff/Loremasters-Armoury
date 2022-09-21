fa3f6a340009531de5aa14b8a864cc3196f47883
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-09-21 15:19:21 -0500

    fix quest completion bug

--- scripts/mods/Loremasters-Armoury/achievements/pickup_maps.lua
+++ scripts/mods/Loremasters-Armoury/achievements/pickup_maps.lua
@@ -2,7 +2,7 @@ local level_quest_table = {
     military = "sub_quest_three_found",
     catacombs = "sub_quest_four_found",
     ussingen = "sub_quest_five_found",
-    dlc_bastion = "sub_quest_06_completed",
+    dlc_bastion = "sub_quest_07_interacted",
 }
 
 return level_quest_table
