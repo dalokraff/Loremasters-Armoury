701b728f4d38b8c6d92e64a219f3df6d04848c2d
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-09-23 15:09:40 -0500

    Update hooks.lua

--- scripts/mods/Loremasters-Armoury/utils/hooks.lua
+++ scripts/mods/Loremasters-Armoury/utils/hooks.lua
@@ -518,7 +518,29 @@ mod:hook_safe(Unit, "animation_event", function(unit, event)
     end
 end)
 
+--for checking if the tomes and grims for sub quest 6 are collected
+mod:hook(AdventureMechanism, "get_end_of_level_rewards_arguments", function (func, self, game_won, quickplay, statistics_db, stats_id)
+    local current_level_key = Managers.level_transition_handler:get_current_level_keys()
+    local collection_levels = require("scripts/mods/Loremasters-Armoury/achievements/official_book_collector")
 
+    if mod:get("sub_quest_five_found") then
+        for level, tisch in pairs(collection_levels) do 
+            if level == current_level_key then 
+                local mission_system = Managers.state.entity:system("mission_system")
+                local tome = mission_system:get_level_end_mission_data("tome_bonus_mission")
+                local grimoire = mission_system:get_level_end_mission_data("grimoire_hidden_mission")
+
+                if tome and grimoire then
+                    if (tome.current_amount == tisch.tomes) and (grimoire.current_amount == tisch.grims) then
+                    mod:set(tisch.quest, true)
+                    end
+                end
+            end
+        end
+    end
+
+    return func(self, game_won, quickplay, statistics_db, stats_id)
+end)
 
 --setting up tables that contain data for the reward info of chalenges in Okri's Book
 mod.LA_quest_rewards = {
@@ -660,27 +682,4 @@ mod:hook(AchievementManager,"setup_achievement_data", function (func, self)
 	end
     local outline = require("scripts/mods/Loremasters-Armoury/achievements/outline")
 	setup_achievement_data_from_categories(self, outline.categories)
-end)
-
-mod:hook(AdventureMechanism, "get_end_of_level_rewards_arguments", function (func, self, game_won, quickplay, statistics_db, stats_id)
-    local current_level_key = Managers.level_transition_handler:get_current_level_keys()
-    local collection_levels = require("scripts/mods/Loremasters-Armoury/achievements/official_book_collector")
-
-    if mod:get("sub_quest_five_found") then
-        for level, tisch in pairs(collection_levels) do 
-            if level == current_level_key then 
-                local mission_system = Managers.state.entity:system("mission_system")
-                local tome = mission_system:get_level_end_mission_data("tome_bonus_mission")
-                local grimoire = mission_system:get_level_end_mission_data("grimoire_hidden_mission")
-
-                if tome and grimoire then
-                    if (tome.current_amount == tisch.tomes) and (grimoire.current_amount == tisch.grims) then
-                    mod:set(tisch.quest, true)
-                    end
-                end
-            end
-        end
-    end
-
-    return func(self, game_won, quickplay, statistics_db, stats_id)
 end)
