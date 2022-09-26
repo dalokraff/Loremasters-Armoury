cd977c9b7a10692facb6e63072d6d659e6a151a0
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-09-26 00:46:07 -0500

    update gem reward icons

--- materials/Loremasters-Armoury/armoury_atlas.lua
+++ materials/Loremasters-Armoury/armoury_atlas.lua
@@ -1,196 +1,196 @@
 -- image source: textures/armoury
 armoury_atlas = { 
-	kerillian_elf_shield_heroclean_saphery01_icon = { 
+	kerillian_elf_shield_heroclean_eataine01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.848633, },
 		uv11 = { 0.373047, 0.930664, },
 	},
-	kruber_bret_shield_basic3_lothar01_purpleglow_icon = { 
+	kruber_empire_shield_basic1_ostermark01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.766602, },
 		uv11 = { 0.373047, 0.848633, },
 	},
-	kerillian_elf_shield_basicclean_eataine01_icon = { 
+	kruber_bret_shield_basic2_luidhard01_purpleglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.684570, },
 		uv11 = { 0.373047, 0.766602, },
 	},
-	kruber_bret_shield_basic2_luidhard01_blueglow_icon = { 
+	kruber_empire_shield_hero1_ostermark01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.602539, },
 		uv11 = { 0.373047, 0.684570, },
 	},
-	kerillian_elf_shield_basic2_eaglegate01_icon = { 
+	kruber_bret_shield_basic2_luidhard01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.520508, },
 		uv11 = { 0.373047, 0.602539, },
 	},
-	kerillian_elf_shield_basicclean_saphery01_icon = { 
+	kruber_bret_shield_hero1_bastonne01_blueglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.438477, },
 		uv11 = { 0.373047, 0.520508, },
 	},
-	la_mq01_reward_sub8_icon = { 
+	kruber_bret_shield_basic3_lothar01_blueglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.356445, },
 		uv11 = { 0.373047, 0.438477, },
 	},
-	bardin_dwarf_shield_heroclean_karaknorn01_axe_icon = { 
+	kruber_bret_shield_basic3_lothar01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.865234, 0.274414, },
 		uv11 = { 0.947266, 0.356445, },
 	},
-	kerillian_elf_bow_antlersong_limbs_autumn_icon = { 
+	kruber_bret_shield_hero1_alberic01_blueglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.783203, 0.274414, },
 		uv11 = { 0.865234, 0.356445, },
 	},
-	kerillian_elf_shield_basic2_griffongate01_icon = { 
+	kruber_bret_shield_hero1_alberic01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.701172, 0.274414, },
 		uv11 = { 0.783203, 0.356445, },
 	},
-	kruber_bret_shield_hero1_alberic01_purpleglow_icon = { 
+	kruber_empire_shield_basic2_ostland01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.619141, 0.274414, },
 		uv11 = { 0.701172, 0.356445, },
 	},
-	kruber_bret_shield_hero1_bastonne01_blueglow_icon = { 
+	kruber_empire_shield_basic3_middenheim01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.537109, 0.274414, },
 		uv11 = { 0.619141, 0.356445, },
 	},
-	kerillian_elf_shield_heroclean_avelorn02_icon = { 
+	kruber_bret_shield_hero1_bastonne01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.455078, 0.274414, },
 		uv11 = { 0.537109, 0.356445, },
 	},
-	kerillian_elf_shield_heroclean_chrace01_icon = { 
+	kruber_bret_shield_hero1_bastonne01_purpleglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.373047, 0.274414, },
 		uv11 = { 0.455078, 0.356445, },
 	},
-	kruber_bret_shield_basic1_reynard01_blueglow_icon = { 
+	kerillian_elf_shield_heroclean_chrace01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.274414, },
 		uv11 = { 0.373047, 0.356445, },
 	},
-	kruber_bret_shield_basic3_lothar01_icon = { 
+	kerillian_elf_shield_heroclean_caledor01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.848633, },
 		uv11 = { 0.291016, 0.930664, },
 	},
-	kruber_bret_shield_basic1_reynard01_icon = { 
+	kruber_empire_shield_basic2_ostland01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.766602, },
 		uv11 = { 0.291016, 0.848633, },
 	},
-	kruber_bret_shield_hero1_alberic01_blueglow_icon = { 
+	kruber_empire_shield_basic1_reiland01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.684570, },
 		uv11 = { 0.291016, 0.766602, },
 	},
-	kruber_bret_shield_basic2_luidhard01_icon = { 
+	kruber_empire_shield_basic1_ostermark01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.602539, },
 		uv11 = { 0.291016, 0.684570, },
 	},
-	kruber_bret_shield_hero1_alberic01_icon = { 
+	kruber_empire_shield_basic2_kotbs01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.520508, },
 		uv11 = { 0.291016, 0.602539, },
 	},
-	kerillian_elf_bow_antlersong_limbs_autumn_glowpurple_icon = { 
+	kruber_bret_shield_basic1_reynard01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.438477, },
 		uv11 = { 0.291016, 0.520508, },
 	},
-	kerillian_elf_shield_heroclean_eataine01_icon = { 
+	kruber_bret_shield_basic1_reynard01_blueglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.356445, },
 		uv11 = { 0.291016, 0.438477, },
 	},
-	kerillian_elf_shield_basicclean_chrace01_icon = { 
+	kruber_bret_shield_hero1_alberic01_purpleglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.274414, },
 		uv11 = { 0.291016, 0.356445, },
 	},
-	kruber_bret_shield_basic2_luidhard01_purpleglow_icon = { 
+	kruber_empire_shield_basic2_middenheim01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.865234, 0.192383, },
 		uv11 = { 0.947266, 0.274414, },
 	},
-	kruber_bret_shield_basic1_reynard01_purpleglow_icon = { 
+	kruber_empire_shield_basic2_kotbs01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.783203, 0.192383, },
 		uv11 = { 0.865234, 0.274414, },
 	},
-	kruber_bret_shield_hero1_bastonne01_icon = { 
+	kruber_empire_shield_hero1_kotbs01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.701172, 0.192383, },
 		uv11 = { 0.783203, 0.274414, },
 	},
-	kruber_bret_shield_basic3_lothar01_blueglow_icon = { 
+	kruber_empire_shield_hero1_kotbs01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.619141, 0.192383, },
 		uv11 = { 0.701172, 0.274414, },
 	},
-	kruber_bret_shield_hero1_bastonne01_purpleglow_icon = { 
+	la_mq01_reward_sub8_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.537109, 0.192383, },
 		uv11 = { 0.619141, 0.274414, },
 	},
-	kerillian_elf_shield_basic_avelorn01_icon = { 
+	kruber_bret_shield_basic1_reynard01_purpleglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.455078, 0.192383, },
 		uv11 = { 0.537109, 0.274414, },
 	},
-	kerillian_elf_shield_basicclean_caledor01_icon = { 
+	la_mq01_reward_sub9_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.373047, 0.192383, },
 		uv11 = { 0.455078, 0.274414, },
 	},
-	bardin_dwarf_shield_basicclean_karaknorn01_axe_icon = { 
+	la_reward_lock_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.192383, },
 		uv11 = { 0.373047, 0.274414, },
 	},
-	bardin_dwarf_shield_basicclean_karaknorn01_hammer_icon = { 
+	bardin_dwarf_shield_basicclean_karaknorn01_axe_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.192383, },
 		uv11 = { 0.291016, 0.274414, },
 	},
-	kerillian_elf_bow_antlersong_limbs_autumn_glowblue_icon = { 
+	kruber_bret_shield_basic3_lothar01_purpleglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.848633, },
 		uv11 = { 0.208984, 0.930664, },
 	},
-	bardin_dwarf_shield_heroclean_karaknorn01_hammer_icon = { 
+	kruber_bret_shield_basic2_luidhard01_blueglow_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.766602, },
 		uv11 = { 0.208984, 0.848633, },
 	},
-	kerillian_elf_shield_heroclean_caledor01_icon = { 
+	kerillian_elf_shield_heroclean_saphery01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.684570, },
 		uv11 = { 0.208984, 0.766602, },
 	},
-	kruber_empire_shield_basic1_ostermark01_mace_icon = { 
+	bardin_dwarf_shield_basicclean_karaknorn01_hammer_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.602539, },
 		uv11 = { 0.208984, 0.684570, },
 	},
-	kruber_empire_shield_basic1_ostermark01_sword_icon = { 
+	kruber_empire_shield_basic1_reiland01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.520508, },
 		uv11 = { 0.208984, 0.602539, },
 	},
-	kruber_empire_shield_basic1_reiland01_mace_icon = { 
+	kruber_empire_shield_basic2_kotbs01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.438477, },
 		uv11 = { 0.208984, 0.520508, },
 	},
-	kruber_empire_shield_basic1_reiland01_sword_icon = { 
+	kruber_empire_shield_basic2_middenheim01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.356445, },
 		uv11 = { 0.208984, 0.438477, },
@@ -200,87 +200,87 @@ armoury_atlas = {
 		uv00 = { 0.126953, 0.274414, },
 		uv11 = { 0.208984, 0.356445, },
 	},
-	kruber_empire_shield_hero1_kotbs01_mace_icon = { 
+	kruber_empire_shield_basic3_middenheim01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.192383, },
 		uv11 = { 0.208984, 0.274414, },
 	},
-	kruber_empire_shield_basic2_middenheim01_mace_icon = { 
+	kerillian_elf_bow_antlersong_limbs_autumn_glowpurple_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.865234, 0.110352, },
 		uv11 = { 0.947266, 0.192383, },
 	},
-	kruber_empire_shield_basic2_middenheim01_sword_icon = { 
+	kerillian_elf_bow_antlersong_limbs_autumn_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.783203, 0.110352, },
 		uv11 = { 0.865234, 0.192383, },
 	},
-	kruber_empire_shield_basic2_ostland01_mace_icon = { 
+	bardin_dwarf_shield_heroclean_karaknorn01_axe_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.701172, 0.110352, },
 		uv11 = { 0.783203, 0.192383, },
 	},
-	kruber_empire_shield_basic2_kotbs01_mace_icon = { 
+	kerillian_elf_shield_basic2_eaglegate01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.619141, 0.110352, },
 		uv11 = { 0.701172, 0.192383, },
 	},
-	kruber_empire_shield_basic2_kotbs01_spear_icon = { 
+	kerillian_elf_bow_antlersong_limbs_autumn_glowblue_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.537109, 0.110352, },
 		uv11 = { 0.619141, 0.192383, },
 	},
-	kruber_empire_shield_basic2_ostland01_sword_icon = { 
+	kerillian_elf_shield_basic2_griffongate01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.455078, 0.110352, },
 		uv11 = { 0.537109, 0.192383, },
 	},
-	kruber_empire_shield_basic1_ostermark01_spear_icon = { 
+	kruber_empire_shield_basic1_reiland01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.373047, 0.110352, },
 		uv11 = { 0.455078, 0.192383, },
 	},
-	kruber_empire_shield_basic2_kotbs01_sword_icon = { 
+	kruber_empire_shield_basic1_ostermark01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.291016, 0.110352, },
 		uv11 = { 0.373047, 0.192383, },
 	},
-	kruber_empire_shield_basic1_reiland01_spear_icon = { 
+	kruber_empire_shield_basic2_middenheim01_mace_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.208984, 0.110352, },
 		uv11 = { 0.291016, 0.192383, },
 	},
-	kruber_empire_shield_basic2_middenheim01_spear_icon = { 
+	bardin_dwarf_shield_heroclean_karaknorn01_hammer_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.126953, 0.110352, },
 		uv11 = { 0.208984, 0.192383, },
 	},
-	kruber_empire_shield_basic3_middenheim01_sword_icon = { 
+	kerillian_elf_shield_basicclean_caledor01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.893555, 0.000000, },
 		uv11 = { 0.975586, 0.082031, },
 	},
-	kruber_empire_shield_basic3_middenheim01_mace_icon = { 
+	kruber_empire_shield_basic3_middenheim01_sword_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.811523, 0.000000, },
 		uv11 = { 0.893555, 0.082031, },
 	},
-	kruber_empire_shield_hero1_kotbs01_sword_icon = { 
+	kerillian_elf_shield_basicclean_eataine01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.729492, 0.000000, },
 		uv11 = { 0.811523, 0.082031, },
 	},
-	kruber_empire_shield_hero1_ostermark01_spear_icon = { 
+	kerillian_elf_shield_basicclean_saphery01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.647461, 0.000000, },
 		uv11 = { 0.729492, 0.082031, },
 	},
-	la_reward_lock_icon = { 
+	kerillian_elf_shield_basic_avelorn01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.565430, 0.000000, },
 		uv11 = { 0.647461, 0.082031, },
 	},
-	kruber_empire_shield_hero1_kotbs01_spear_icon = { 
+	kerillian_elf_shield_basicclean_chrace01_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.483398, 0.000000, },
 		uv11 = { 0.565430, 0.082031, },
@@ -290,7 +290,7 @@ armoury_atlas = {
 		uv00 = { 0.401367, 0.000000, },
 		uv11 = { 0.483398, 0.082031, },
 	},
-	kruber_empire_shield_basic3_middenheim01_spear_icon = { 
+	kruber_empire_shield_hero1_kotbs01_spear_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.319336, 0.000000, },
 		uv11 = { 0.401367, 0.082031, },
@@ -300,7 +300,7 @@ armoury_atlas = {
 		uv00 = { 0.237305, 0.000000, },
 		uv11 = { 0.319336, 0.082031, },
 	},
-	la_mq01_reward_sub9_icon = { 
+	kerillian_elf_shield_heroclean_avelorn02_icon = { 
 		size = { 84, 84, },
 		uv00 = { 0.000000, 0.892578, },
 		uv11 = { 0.082031, 0.974609, },
@@ -315,32 +315,32 @@ armoury_atlas = {
 		uv00 = { 0.000000, 0.767578, },
 		uv11 = { 0.124023, 0.892578, },
 	},
-	la_mq01_quest_sub4_icon = { 
+	la_mq01_quest_sub3_icon = { 
 		size = { 130, 131, },
 		uv00 = { 0.000000, 0.639648, },
 		uv11 = { 0.126953, 0.767578, },
 	},
-	la_mq01_quest_sub5_icon = { 
+	la_quest_lock_icon = { 
 		size = { 130, 131, },
 		uv00 = { 0.000000, 0.511719, },
 		uv11 = { 0.126953, 0.639648, },
 	},
-	loremaster_test_icon = { 
+	la_mq01_quest_sub5_icon = { 
 		size = { 130, 131, },
 		uv00 = { 0.000000, 0.383789, },
 		uv11 = { 0.126953, 0.511719, },
 	},
-	la_quest_lock_icon = { 
+	la_mq01_quest_sub4_icon = { 
 		size = { 130, 131, },
 		uv00 = { 0.000000, 0.255859, },
 		uv11 = { 0.126953, 0.383789, },
 	},
-	la_mq01_quest_sub3_icon = { 
+	loremaster_test_icon_old = { 
 		size = { 130, 131, },
 		uv00 = { 0.000000, 0.127930, },
 		uv11 = { 0.126953, 0.255859, },
 	},
-	loremaster_test_icon_old = { 
+	loremaster_test_icon = { 
 		size = { 130, 131, },
 		uv00 = { 0.000000, 0.000000, },
 		uv11 = { 0.126953, 0.127930, },
