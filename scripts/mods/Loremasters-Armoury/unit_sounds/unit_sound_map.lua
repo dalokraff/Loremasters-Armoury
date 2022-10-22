cae46f82e0f9fc0c9b1360528381cdd687eb797d
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-10-22 14:10:29 -0500

    Update unit_sound_map.lua

--- scripts/mods/Loremasters-Armoury/unit_sounds/unit_sound_map.lua
+++ scripts/mods/Loremasters-Armoury/unit_sounds/unit_sound_map.lua
@@ -131,4 +131,229 @@ unit_sound_map["units/empire_sword/Kruber_KOTBS_empire_sword_01_mesh_gold"] = {
     },
 }
 
+unit_sound_map["units/bret_sword/Kruber_kotbs_longsword_mesh"] = {
+    to_bastard_sword = {
+        name = "weapon_foley_equip_sword_2h",
+    },
+    attack_swing_up_left = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_right = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_down = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_down_right ={
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_heavy_left_diagonal = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_heavy_right_diagonal = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_heavy_down = {
+        name = "rare_sword_2h_swing",
+    },
+}
+
+unit_sound_map["units/bret_sword/Kruber_kotbs_longsword_mesh_gold"] = {
+    to_bastard_sword = {
+        name = "weapon_foley_equip_sword_2h",
+    },
+    attack_swing_up_left = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_right = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_down = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_down_right ={
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_heavy_left_diagonal = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_heavy_right_diagonal = {
+        name = "rare_sword_2h_swing",
+    },
+    attack_swing_heavy_down = {
+        name = "rare_sword_2h_swing",
+    },
+}
+
+unit_sound_map["units/wizard_sword/Sienna_kotbs_sword_mesh"] = {
+    to_1h_sword = {
+        name = "weapon_foley_equip_sword_1h",
+    },
+    attack_swing_down = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left_diagonal = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right_diagonal = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_heavy = {
+        name = "rare_sword_1h_charge_swing",
+    },
+    attack_swing_heavy_left = {
+        name = "rare_sword_1h_charge_swing",
+    },
+    attack_swing_heavy_right = {
+        name = "rare_sword_1h_charge_swing",
+    },
+    attack_swing_heavy_stab = {
+        name = "swing_1h_broadsword",
+    },
+    attack_swing_stab = {
+        name = "swing_1h_broadsword",
+    },
+}
+
+unit_sound_map["units/wizard_sword/Sienna_kotbs_sword_mesh_gold"] = {
+    to_1h_sword = {
+        name = "weapon_foley_equip_sword_1h",
+    },
+    attack_swing_down = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left_diagonal = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right_diagonal = {
+        name = "rare_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_heavy = {
+        name = "rare_sword_1h_charge_swing",
+    },
+    attack_swing_heavy_left = {
+        name = "rare_sword_1h_charge_swing",
+    },
+    attack_swing_heavy_right = {
+        name = "rare_sword_1h_charge_swing",
+    },
+    attack_swing_heavy_stab = {
+        name = "swing_1h_broadsword",
+    },
+    attack_swing_stab = {
+        name = "swing_1h_broadsword",
+    },
+}
+
+unit_sound_map["units/wizard_sword/Sienna_kotbs_flame_sword_mesh"] = {
+    to_1h_sword = {
+        name = "weapon_foley_equip_sword_1h",
+    },
+    attack_swing_down = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left_diagonal = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right_diagonal = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_heavy = {
+        name = "fire_sword_special_swing",
+    },
+    attack_swing_heavy_left = {
+        name = "fire_sword_special_swing",
+    },
+    attack_swing_heavy_right = {
+        name = "fire_sword_special_swing",
+    },
+    attack_swing_heavy_stab = {
+        name = "swing_1h_broadsword",
+    },
+    attack_swing_stab = {
+        name = "flaming_sword_1h_swing",
+    },
+    attack_swing_charge = {
+        name = "Play_fire_hand_charge_sword",
+    },
+}
+
+unit_sound_map["units/wizard_sword/Sienna_kotbs_flame_sword_gold_mesh"] = {
+    to_1h_sword = {
+        name = "weapon_foley_equip_sword_1h",
+    },
+    attack_swing_down = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_left_diagonal = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_right_diagonal = {
+        name = "flaming_sword_1h_swing",
+        delay = 0.1,
+    },
+    attack_swing_heavy = {
+        name = "fire_sword_special_swing",
+    },
+    attack_swing_heavy_left = {
+        name = "fire_sword_special_swing",
+    },
+    attack_swing_heavy_right = {
+        name = "fire_sword_special_swing",
+    },
+    attack_swing_heavy_stab = {
+        name = "swing_1h_broadsword",
+    },
+    attack_swing_stab = {
+        name = "flaming_sword_1h_swing",
+    },
+    attack_swing_charge = {
+        name = "Play_fire_hand_charge_sword",
+    },
+}
+
+
 return unit_sound_map
