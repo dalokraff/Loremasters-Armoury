c2932e55cfd94a816c0b2533e4a4505aca211d7e
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-10-29 18:15:39 -0500

    add overcharge generation buff for quest 8

--- scripts/mods/Loremasters-Armoury/buffs/LA_buffs.lua
+++ scripts/mods/Loremasters-Armoury/buffs/LA_buffs.lua
@@ -21,21 +21,21 @@ local test_buff_templates = {
 				bonus = 2,
 			}
 		}
+	},
+	sub_quest_08_heatgen_buff = {
+		buffs = {
+			{
+				max_stacks = 1,
+				icon = "la_buff_01_icon",
+				stat_buff = "reduced_overcharge",
+				multiplier = -0.9,
+			}
+		}
 	}
 }
 
 for buff_name, data in pairs(test_buff_templates) do
     BuffTemplates[buff_name] = data
 end
--- BuffTemplates["loremasters_02_buff"] = test_buff_templates.loremasters_01_buff
+
 BuffUtils.copy_talent_buff_names(test_buff_templates)
-
-
--- local params = {
---     external_optional_duration = 10,
--- }
--- local player = Managers.player:local_player()
--- local player_unit = player.player_unit   
--- local buff_extension = ScriptUnit.extension(player_unit, "buff_system")
--- buff_extension:add_buff("sub_quest_08_cdr_buff", params)
--- buff_extension:add_buff("sub_quest_08_stamina_buff", params)
