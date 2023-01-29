d0f717dba633ec1821e30fabe123ee6fe76a4ee3
dalokraff <87997987+dalokraff@users.noreply.github.com>
2023-01-29 17:58:08 -0600

    add retexture testing tools

--- scripts/mods/Loremasters-Armoury/Loremasters-Armoury.lua
+++ scripts/mods/Loremasters-Armoury/Loremasters-Armoury.lua
@@ -636,3 +636,31 @@ end
 --         end
 --     end
 -- end
+
+
+mod:dofile("scripts/mods/Loremasters-Armoury/cosmetic_testing_tools/retexture_mesh")
+
+
+mod:command("gk_body_mesh", "", function(diffuse, normal, combined)
+    local player = Managers.player:local_player()
+    local player_unit = player.player_unit
+    local position = Unit.local_position(player_unit, 0)
+    local rotation = Unit.local_rotation(player_unit, 0)
+    local altered_mesh = RetextureMesh:new("units/beings/player/empire_soldier_knight/third_person_base/chr_third_person_mesh", Managers.state.unit_spawner, position, rotation)
+    altered_mesh:set_texture("texture_map_64cc5eb8", diffuse)
+    altered_mesh:set_texture("texture_map_861dbfdc", normal)
+    altered_mesh:set_texture("texture_map_abb81538", combined)
+end)
+
+
+Managers.package:load("units/beings/player/empire_soldier_breton/headpiece/es_gk_hat_03", "global")
+mod:command("gk_hat_mesh", "", function(diffuse, normal, combined)
+    local player = Managers.player:local_player()
+    local player_unit = player.player_unit
+    local position = Unit.local_position(player_unit, 0) +Vector3(0,0,1)
+    local rotation = Unit.local_rotation(player_unit, 0)
+    local altered_mesh = RetextureMesh:new("units/beings/player/empire_soldier_breton/headpiece/es_gk_hat_03", Managers.state.unit_spawner, position, rotation)
+    altered_mesh:set_texture("texture_map_c0ba2942", diffuse)
+    altered_mesh:set_texture("texture_map_59cd86b9", normal)
+    altered_mesh:set_texture("texture_map_0205ba86", combined)
+end)
---
+++ scripts/mods/Loremasters-Armoury/cosmetic_testing_tools/retexture_mesh.lua
@@ -0,0 +1,43 @@
+local mod = get_mod("Loremasters-Armoury")
+
+RetextureMesh = class(RetextureMesh)
+
+RetextureMesh.init = function (self, unit_path, unit_spawner, pos, rot)
+
+    self.world = Managers.world:world("level_world")
+    self.unit_path = unit_path
+    self.unit_spawner = unit_spawner
+    self.unit = unit_spawner:spawn_local_unit(unit_path, pos, rot)
+
+end
+
+RetextureMesh.set_texture = function(self, text_slot, texture_file, meshes_to_skip)
+    local skip_meshes = meshes_to_skip or {}
+
+    local unit = self.unit
+
+    if Unit.alive(unit) and texture_file and text_slot then
+        local num_meshes = Unit.num_meshes(unit)
+        for i = 0, num_meshes - 1, 1 do
+            if not skip_meshes[i] then
+                local mesh = Unit.mesh(unit, i)
+                local num_mats = Mesh.num_materials(mesh)
+                for j = 0, num_mats - 1, 1 do
+                    local mat = Mesh.material(mesh, j)
+                    Material.set_texture(mat, text_slot, texture_file)
+                end
+            end
+        end
+    end    
+end
+
+RetextureMesh.destroy = function(self) 
+
+    self.unit_spawner:mark_for_deletion(self.unit)
+
+    self.world = nil
+    self.unit_path = nil
+    self.unit_spawner = nil
+    self.unit = nil
+
+end
